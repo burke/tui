0074d994f692fc5885714fbb9f6dff82ca8afa5d
Burke Libbey <burke@libbey.me>
2021-10-07 16:46:32 -0400

    .

--- lib/tui/profile/ansi.rb
+++ lib/tui/profile/ansi.rb
@@ -8,12 +8,24 @@ module TUI
       extend(T::Sig)
       extend(Profile)
 
-      sig { override.params(c: Color).returns(Color::ANSIColor) }
+      sig { override.params(c: Color).returns(Color) }
       def self.convert(c)
-        Color::ANSIColor.new(0)
+        c
+        # case c
+        # when Color::NoColor
+        #   c
+        # when Color::ANSIColor
+        #   c
+        # when Color::ANSI256Color
+        #   # c.to_ansi
+        #   c
+        # when Color::RGBColor
+        #   # c.to_ansi
+        #   c
+        # end
       end
 
-      sig { override.params(s: String).returns(Color::ANSIColor) }
+      sig { override.params(s: String).returns(Color) }
       def self.color(s)
         Color::ANSIColor.new(0)
       end
